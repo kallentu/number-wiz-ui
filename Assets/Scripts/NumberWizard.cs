158aeeff37572843bd1303835bdb38845ac65172
Kallen Tu <kallentu@hotmail.ca>
2016-08-24 14:05:43 -0700

    Added initial files

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+	
+	int maxGuessesAllowed = 17;
+	
+	public Text theguess;
+
+	// Use this for initialization - beginning of game
+	void Start () {
+		StartGame();
+		StartGame2();
+		StartGame3();
+		theguess.text = guess.ToString();
+	}
+		
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+			
+	}	
+	
+	public void GuessLower() {
+		max = guess;
+		NextGuess ();
+	
+	}
+	
+
+	public void StartGame() {
+		
+		min = 1;
+		max = 1000;			
+		guess = (Random.Range(min, max)) ;
+		
+		max = max + 1 ;
+		
+	}
+	
+	public void StartGame2() {
+		
+		min = 1;
+		max = 10000;
+		guess = (Random.Range(min, max)) ;
+		
+		max = max + 1 ;
+	
+	}
+	
+	public void StartGame3() {
+		
+		min = 1;
+		max = 30000;
+		guess = (Random.Range(min, max)) ;
+	
+		max = max + 1 ;
+		
+	}
+
+
+		
+	void NextGuess() {
+		
+		guess = (Random.Range(min, max)) ;
+		theguess.text = guess.ToString(); //changes guess to be a string that will show up on screen
+		//theguess is a variable and .text is an attribute of that variable
+		
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Range3.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Range3 : MonoBehaviour {
+	
+	int max;
+	int min;
+	int guess;
+	
+	int maxGuessesAllowed = 17;
+	
+	public Text theGuess;
+	public Text numberLeft;
+	
+	// Use this for initialization - beginning of game
+	void Start () {
+		StartGame();
+		theGuess.text = guess.ToString();
+	}
+	
+	
+	
+	public void GuessHigher() {
+		if (guess == 30000)
+		{
+			Application.LoadLevel("Cheat");
+		}
+		else
+		{
+			min = guess;
+			NextGuess ();
+		}
+	}	
+	
+	public void GuessLower() {
+		if (guess == 1)
+		{
+			Application.LoadLevel("Cheat");
+		}
+		else
+		{
+			max = guess;
+			NextGuess ();
+		}
+	}
+	
+	
+	public void StartGame() {
+		
+		min = 0;
+		max = 30001;			
+		guess = (Random.Range(min,max));
+		
+		max = max + 1 ;
+		
+	}
+	
+	
+	
+	
+	void NextGuess() {
+		
+		guess = (Random.Range((min + 1), (max - 1))) ;
+		theGuess.text = guess.ToString(); //changes guess to be a string that will show up on screen
+		//theguess is a variable and .text is an attribute of that variable
+		
+		
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		numberLeft.text = maxGuessesAllowed.ToString(); //shows how many guesses are left before you fail
+		
+		if (guess >= max || guess <= min) {
+			Application.LoadLevel("Cheat");
+		}
+		
+		if (maxGuessesAllowed < 0) {
+			Application.LoadLevel("Win");
+			
+		}
+
+		
+	}
+}
